039c994dfc613cd0df2e083054d98bf1abf9cc6b
thisaru <amithawerahena@gmail.com>
2024-09-01 10:34:04 +0530

    third commit

--- src/main/java/lk/ijse/assignment1/user/Kashul.java
+++ src/main/java/lk/ijse/assignment1/user/Kashul.java
@@ -3,13 +3,16 @@ package lk.ijse.assignment1.user;
 
 import lk.ijse.assignment1.foods.Food;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Kashul {
-
+    @Qualifier
     Food myFavouriteFood ;
+    @Autowired
     public Kashul(){
         System.out.println("my favourite food is" + myFavouriteFood );
     }
-
 
 }
